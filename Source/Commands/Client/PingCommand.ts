5796bb61c1e8a500cdd029fd9d974dc7d4896989
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-01 18:08:07 +0530

    Made improvisations to Ping Command

--- Source/Commands/Client/PingCommand.ts
+++ Source/Commands/Client/PingCommand.ts
@@ -1,10 +1,24 @@
+import { stripIndents } from "common-tags";
+import { MessageEmbed } from "discord.js";
 import { Command } from "../../Types/Command";
 
 const PingCommand: Command = {
 	name: "ping",
 	description: "Gets the API Latency of the Bot",
-	run(client, message) {
-		return message.channel.send(client.ws.ping);
+	async run(client, message) {
+		const msg = await message.channel.send("Pinging...");
+
+		const PingEmbed = new MessageEmbed()
+			.setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+			.setTitle("API Latency")
+			.setDescription(stripIndents`
+				The Websocket Ping of the Bot is: ${client.ws.ping} ms
+				The Heartbeart 💝 of the bot is: ${msg.createdTimestamp - message.createdTimestamp} ms
+			`)
+			.setColor("GREEN");
+
+		message.channel.send(PingEmbed)
+			.then(() => msg.delete());
 	},
 };
 
