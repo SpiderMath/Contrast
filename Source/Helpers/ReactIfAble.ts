a8a0405ea2ae1d942dea05ee296ac6bed5d69132
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-03 05:57:41 +0530

    Restarted shtuff again :facepalm:

--- Source/Helpers/ReactIfAble.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from "discord.js";
-
-export default function ReactIfAble(message: Message, emoji: string) {
-	// @ts-ignore
-	if(message.guild && !message.channel.permissionsFor(message.client.user).has("ADD_REACTIONS")) return;
-
-	message.react(emoji);
-}
